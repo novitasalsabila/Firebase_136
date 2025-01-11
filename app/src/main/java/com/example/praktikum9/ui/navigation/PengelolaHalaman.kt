f90f04ba0d2fb3b981ced208cc9b4780be0bf27f
novitasalsabila <novnovv10@gmail.com>
2025-01-11 14:41:04 +0700

    mengatur navigasi antar halaman. pengguna bisa berpindah antara halaman beranda (HomeView) dan halaman tambah mahasiswa (InsertView).

---
+++ app/src/main/java/com/example/praktikum9/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.praktikum9.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum9.ui.pages.HomeView
+import com.example.praktikum9.ui.pages.InsertView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
